376ec264b8ba94c16a2f9af4a6a5aa7f7aa0877e
Plaris-algedi <karchalmohammed@gmail.com>
2023-09-22 16:01:52 -0700

    Add all tasks

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,11 @@
+# installs the package flask from pip3
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}

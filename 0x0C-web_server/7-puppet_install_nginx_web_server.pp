caa6ea8561f4477e6a466463a667af5245f41cc5
Plaris-algedi <karchalmohammed@gmail.com>
2023-09-26 18:14:35 -0700

    Add a puppet manifest that install configure nginx

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,21 @@
+# Install Nginx web server (w/ Puppet)
+
+package { 'nginx':
+  ensure => 'installed',
+}
+
+file {'/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+file_line {'configure redirection':
+  path  =>  '/etc/nginx/sites-available/default',
+  after =>  'server_name _;',
+  line  =>  "\n\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n\t}\n",
+}
+
+service { 'nginx':
+  ensure => running,
+  enable => true,
+  subscribe => File['/etc/nginx/sites-available/default'],
+}

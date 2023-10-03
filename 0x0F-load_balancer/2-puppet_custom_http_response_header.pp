67625176500ff38a561c26fcba38aa2461afc869
Plaris-algedi <karchalmohammed@gmail.com>
2023-10-02 19:50:28 -0700

    Use shell commands instead

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,46 +1,9 @@
 # Automate the task of creating a custom HTTP header response
 
-$config="events {}
-
-http {
-
-    server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-
-	add_header X-Served-By $HOSTNAME;
-
-        server_name purepolaris.tech;
-
-        root /var/www/html/test;
-        index index.html;
-
-        location /redirect_me {
-            return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-        }
-        
-        error_page 404 /custom_404.html;
-        location = /custom_404.html {
-            internal;
-        }
-    }
-}"
-
-package { 'nginx':
-  ensure => installed,
-  before => File['/etc/nginx/nginx.conf'],
-}
-
-
-file { '/etc/nginx/nginx.conf':
-  ensure  => present,
-  content => $config,
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/nginx/nginx.conf'],
+exec { 'add X-Served-By header':
+  command  => 'sudo apt-get -y update;
+  sudo apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  sudo service nginx restart',
+  provider => shell,
 }

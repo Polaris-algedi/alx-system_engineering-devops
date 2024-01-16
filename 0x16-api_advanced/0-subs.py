84957bd382ca66d78e2b28224863233e2dc1fe4d
Polaris-algedi <karchalmohammed@gmail.com>
2024-01-16 08:14:20 -0800

    Change the user-agent

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    headers = {'User-Agent': 'custom'}
+    headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(f'https://www.reddit.com/r/{subreddit}/about.json',
                             headers=headers, allow_redirects=False)
 

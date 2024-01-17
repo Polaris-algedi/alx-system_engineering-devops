4a48827f8d6fd6fc518941fef1c58239683f9856
Polaris-algedi <102882133+Polaris-algedi@users.noreply.github.com>
2024-01-17 20:23:52 +0100

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# This Puppet manifest replaces 'phpp' with 'php' in /var/www/html/wp-settings.php
-exec { 'replace_phpp_with_php':
-  command => '/bin/sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
-  onlyif  => '/bin/grep -q phpp /var/www/html/wp-settings.php',
-  path    => ['/bin', '/usr/bin'],
-}
